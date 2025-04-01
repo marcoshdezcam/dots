8e70c08fa5a58f94101d2c0b41923352570af855
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2025-04-01 13:49:34 -0600

    Nix Dev env setup on NVIM.

---
+++ nvim/lua/config/lsp-nix.lua
@@ -0,0 +1,26 @@
+return {
+  opts = function()
+    require("lspconfig").nixd.setup({
+  cmd = { "nixd" },
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      formatting = {
+        command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
+      },
+      -- options = {
+      --   nixos = {
+      --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
+      --   },
+      --   home_manager = {
+      --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
+      --   },
+      -- },
+    },
+  },
+})
+  end
+}
+

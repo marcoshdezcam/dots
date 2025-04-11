04223715f8087161635ac13660be6bc9ab0377c5
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2025-04-11 00:47:11 -0600

    Add floating terminal on Hyprland

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default =
+      pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            neovim
+	    nodejs_22
+          ];
+
+          shellHook = ''
+            echo "Dots Environment"
+	    zsh
+          '';
+        };
+  };
+}
